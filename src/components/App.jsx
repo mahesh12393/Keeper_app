a42841c892350c9f68bd78efb07da977a798ce0b
Maheswar147 <preddy@ec.iitr.ac.in>
2021-02-16 13:17:12 +0530

    Added footer of the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Heading from "./Heading.jsx";
+import Footer from "./Footer.jsx"
 function App(){
     return <div>
    <Heading></Heading>
+   <Footer></Footer>
     </div>
 }
 export default App
