dcda45a394cda2aea89479b4fbb2bb2b58dcc760
Maheswar147 <preddy@ec.iitr.ac.in>
2021-02-20 22:39:43 +0530

    added several notes of the project

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-function Note() {
+import notes from "../notes.js";
+function Note(props) {
   return (
     <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note body</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
+
 export default Note;
